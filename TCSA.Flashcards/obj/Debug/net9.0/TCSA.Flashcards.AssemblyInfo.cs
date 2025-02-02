32d57a3281852e2c6d252d337df0d3fe3217279c
Hashim Zafar <hashimbinzafar@hotmail.com>
2025-02-02 14:55:26 -0500

    werwer

--- TCSA.Flashcards/obj/Debug/net9.0/TCSA.Flashcards.AssemblyInfo.cs
+++ TCSA.Flashcards/obj/Debug/net9.0/TCSA.Flashcards.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TCSA.Flashcards")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22865bf3d56eca9db1ae36594cf6ff40941d48e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1eed2f2737b99add57302e87263092ff6ace1f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TCSA.Flashcards")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TCSA.Flashcards")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
